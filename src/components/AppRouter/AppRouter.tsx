5584512384158229785df355857270f0905e135a
VadimLebedev <Merzlyy.Pes@yandex.ru>
2024-06-30 17:02:23 +0300

    task 3: Маршрут по тайным тропам: Добавил приватные роуты, внес правки по прошлым комментариям, вынес типы в отдельный файл

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Header from '../Header/Header';
+import React, {Fragment} from "react";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {CardsCityProps} from "../../types/commonTypes";
 import MainPage from '../../pages/MainPage/MainPage';
 import FavoritesPage from '../../pages/FavoritesPage/FavoritesPage';
-
-import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "../Layout/Layout";
-import {CardsCityProps} from "../../types/commonTypes";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import LoginPage from "../../pages/LoginPage/LoginPage";
+import Room from "../../pages/Room/Room";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 interface AppRouterProps {
   cardsMain: CardsCityProps[];
@@ -17,10 +19,37 @@ interface AppRouterProps {
 const AppRouter: React.FC<AppRouterProps> = ({cardsMain, cardsFavorites, cardsAnotherCity}) => {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route index element={<MainPage cards={cardsMain} />}/>
+          <Route index element={<MainPage cards={cardsMain}/>}/>
 
+          <PrivateRoute>
+            <Route path='favorites'
+                   element={
+                     <FavoritesPage
+                       cards={cardsFavorites}
+                       cardsAnotherCity={cardsAnotherCity}/>
+                   }/>
+          </PrivateRoute>
+
+          <Route path='offer'>
+            <Route index element={<Room/>}/>
+            <Route path=':id' element={<Room/>}/>
+          </Route>
+
+          <Route path='login' element={<LoginPage/>}/>
+
+          <Route path='*' element={
+            <Fragment>
+              <h1>
+                404.
+                <br/>
+                <small>Page not found</small>
+              </h1>
+              <Link to="/">Go to main page</Link>
+            </Fragment>
+          }/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Room/Room.tsx
@@ -0,0 +1,7 @@
+const Room = (): JSX.Element => {
+  return (
+    <></>
+  )
+}
+
+export default Room;
